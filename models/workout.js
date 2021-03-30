ba5a4a015bdec0fdc60270323586229deefbf15b
adamjuwig <adamjuwig@gmail.com>
2021-03-30 10:23:46 -0700

    trying to fix DB issues

---
+++ models/workout.js
@@ -0,0 +1,52 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const WorkoutSchema = new Schema ({
+    day: {
+        type: Date, default: () => new Date()
+        
+    },
+    exercises: [
+        {
+
+        name: {
+            type: String,            
+            required: true,
+            trim: true,
+        },
+        type: {
+            type: String,
+            required: true,
+            trim: true,
+        },
+        duration: {
+            type: Number,
+            required: true,
+        },
+        weight: {
+            type: Number,            
+        },
+        reps: {
+            type: Number,            
+        },
+        sets: {
+            type: Number,                    
+        },
+        distance: {
+            type: Number,            
+        },
+      },
+    ],
+    
+    toJSON: {
+        virtuals: true,
+    },
+});
+
+WorkoutSchema.virtual("totalDuration").get(() => {
+    return this.exercises.reduce((total, exercise) => total + exercise.duration, 0);
+});
+
+const Workout = mongoose.model("Workout", workoutSchema);
+
+module.exports = Workout;
---
+++ routes/api-routes.js
@@ -0,0 +1,53 @@
+const router = require("express").Router();
+const { Workout } = require("/models");
+const db = require("/models");
+
+router.get("/api/workouts", async function (req, res) {
+Workout.find().then(function (results) {
+    res.json(results)
+})
+});
+
+router.post("/api/workouts", async function (req, res) {
+  try {
+    res.json(await db.Workout.create({}));
+  } catch (err) {
+    res.status(500).end();
+  }
+});
+
+router.put("/api/workouts/", async function (req, res) {
+  try {
+    res.json(
+     db.Workout.findByIdAndUpdate(
+        req.params.id,
+        { $push: { exercises: req.body } },
+        { new: true, runValidators: true }
+      )
+    );
+  } catch (err) {
+    res.status(500).end();
+  }
+});
+
+router.get("/api/workouts", async function (req, res) {
+  try {
+    res.json((
+      await Workout.aggregate([
+        {
+          $addFields: {
+            totalDuration: {
+              $sum: "$exercises.duration",
+            },
+          },
+        },
+      ])
+        .sort({ day: -1 })
+        .limit(7)
+    ). reverse());
+  } catch (err) {
+    res.status(500).end();
+  }
+});
+
+module.exports = router;
